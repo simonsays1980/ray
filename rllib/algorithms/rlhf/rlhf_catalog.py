4d782ddbaa4b8b90be321b089972cba1131a2546
simonsays1980 <simon.zehnder@gmail.com>
2024-11-22 11:47:58 +0100

    Securing intermediate results after crash.
    Signed-off-by: simonsays1980 <simon.zehnder@gmail.com>

---
+++ rllib/algorithms/rlhf/rlhf_catalog.py
@@ -0,0 +1,22 @@
+import gymnasium as gym
+from ray.rllib.algorithms.ppo.ppo_catalog import PPOCatalog
+from ray.rllib.core.models.configs import LLMConfig
+
+class RLHFCatalog(PPOCatalog):
+
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        action_space: gym.Space,
+        model_config_dict: dict,
+    ):
+
+        super().__init__(
+            observation_space=observation_space,
+            action_space=action_space,
+            model_config_dict=model_config_dict
+        )
+
+        self.policy = LLMConfig(
+            model_id=self._model_config_dict["sft_model_id"],
+        )
