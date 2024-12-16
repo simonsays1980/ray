4a803548ce5c429ff4948991b33ee53a670173e4
simonsays1980 <simon.zehnder@gmail.com>
2024-12-16 15:53:43 +0100

    Added test for TLDR RLHF OfflineData.
    Signed-off-by: simonsays1980 <simon.zehnder@gmail.com>

---
+++ rllib/tuned_examples/rlhf/tests/test_tldr_rlhf_offline_data.py
@@ -0,0 +1,20 @@
+from ray.rllib.algorithms.rlhf.rlhf import RLHFConfig
+from ray.rllib.tuned_examples.rlhf.tldr_rlhf_offline_data import TLDRRLHFOfflineData
+
+config = (
+    RLHFConfig()
+    .offline_data(
+        input_="trl-internal-testing/tldr-preference-sft-trl-style",
+        iter_batches_kwargs={
+            "local_shuffle_buffer_size": None,
+        }
+    )
+    .training(
+        sft_model_id="cleanrl/EleutherAI_pythia-1b-deduped__sft__tldr",
+    )
+)
+
+offline_data = TLDRRLHFOfflineData(config)
+
+for i in range(10):
+    batch = offline_data.sample(num_samples=4)
